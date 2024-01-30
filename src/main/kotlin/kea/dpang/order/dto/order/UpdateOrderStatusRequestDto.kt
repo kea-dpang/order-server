79c2503cc93045928142bd73c71e2b57d7abbed1
SeungHyeon <namsh1125@naver.com>
2024-01-31 00:16:47 +0900

    Feat: OrderStatus에 대한 커스텀 컨버터 구현 및 적용

--- src/main/kotlin/kea/dpang/order/dto/order/UpdateOrderStatusRequestDto.kt
+++ src/main/kotlin/kea/dpang/order/dto/order/UpdateOrderStatusRequestDto.kt
@@ -1,5 +1,7 @@
 package kea.dpang.order.dto.order
 
+import jakarta.persistence.Convert
+import kea.dpang.order.converter.StringToOrderStatusConverter
 import kea.dpang.order.entity.OrderStatus
 
 /**
@@ -8,5 +10,6 @@ import kea.dpang.order.entity.OrderStatus
  * @property status 변경할 상태
  */
 data class UpdateOrderStatusRequestDto(
+    @Convert(converter = StringToOrderStatusConverter::class)
     val status: OrderStatus
 )
