e9c6887ea51d9446e831c589f96301be606ea422
SeungHyeon <namsh1125@naver.com>
2024-02-06 15:42:37 +0900

    !HOTFIX: 상품 정보를 제공할 때, 상품 식별자를 포함하여 응답

--- src/main/kotlin/kea/dpang/order/dto/ProductInfoDto.kt
+++ src/main/kotlin/kea/dpang/order/dto/ProductInfoDto.kt
@@ -5,12 +5,14 @@ import kea.dpang.order.feign.dto.ItemInfoDto
 /**
  * 상품 정보를 담는 DTO
  *
+ * @property itemId 상품 ID
  * @property image 상품 이미지
  * @property name 상품명
  * @property price 상품 금액
  */
 
 data class ProductInfoDto(
+    val itemId: Long,
     val image: String,
     val name: String,
     val price: Int
@@ -18,6 +20,7 @@ data class ProductInfoDto(
     companion object {
         fun from(itemInfoDto: ItemInfoDto): ProductInfoDto {
             return ProductInfoDto(
+                itemId = itemInfoDto.id,
                 image = itemInfoDto.thumbnailImage,
                 name = itemInfoDto.name,
                 price = itemInfoDto.price
