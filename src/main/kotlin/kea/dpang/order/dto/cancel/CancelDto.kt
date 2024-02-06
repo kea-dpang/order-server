3811074ce52393f3a4808a5ab88d307afb581024
SeungHyeon <namsh1125@naver.com>
2024-02-06 19:59:34 +0900

    Update: 주문 취소 정보에 사용자 정보 추가해서 응답

--- src/main/kotlin/kea/dpang/order/dto/cancel/CancelDto.kt
+++ src/main/kotlin/kea/dpang/order/dto/cancel/CancelDto.kt
@@ -7,6 +7,7 @@ import java.time.LocalDate
  * 취소 정보를 담는 DTO
  *
  * @property cancelId 취소 식별자
+ * @property userId 사용자 식별자
  * @property cancelRequestDate 취소 신청일
  * @property orderId 주문 식별자
  * @property orderDate 주문 일자
@@ -15,6 +16,7 @@ import java.time.LocalDate
  */
 data class CancelDto(
     val cancelId: Long,
+    val userId: Long,
     val cancelRequestDate: LocalDate,
     val orderId: Long,
     val orderDate: LocalDate,
