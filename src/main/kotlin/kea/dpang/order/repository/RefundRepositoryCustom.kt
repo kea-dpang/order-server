cce797ae080a989860973ff29a41b032862b9767
SeungHyeon <namsh1125@naver.com>
2024-02-10 01:15:52 +0900

    Update: 필터링 조건 변경 (환불 사유 -> 환불 상태)

--- src/main/kotlin/kea/dpang/order/repository/RefundRepositoryCustom.kt
+++ src/main/kotlin/kea/dpang/order/repository/RefundRepositoryCustom.kt
@@ -1,7 +1,7 @@
 package kea.dpang.order.repository
 
-import kea.dpang.order.entity.RefundReason
 import kea.dpang.order.entity.Refund
+import kea.dpang.order.entity.RefundStatus
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import java.time.LocalDate
@@ -14,17 +14,17 @@ fun interface RefundRepositoryCustom {
     /**
      * 주어진 검색 조건과 페이징 정보에 따라 환불 목록을 조회합니다.
      *
-     * @param startDate   조회할 환불의 시작 날짜
-     * @param endDate     조회할 환불의 종료 날짜
-     * @param refundReason      조회할 환불의 사유
-     * @param userId      사용자 식별자
-     * @param pageable    페이징 정보를 나타내는 [Pageable] 객체
+     * @param startDate    조회할 환불의 시작 날짜
+     * @param endDate      조회할 환불의 종료 날짜
+     * @param refundStatus 조회할 환불의 상태
+     * @param userId       사용자 식별자
+     * @param pageable     페이징 정보를 나타내는 [Pageable] 객체
      * @return [Page] 인터페이스를 구현한 객체를 통해 페이징 처리된 환불 목록
      */
     fun findRefunds(
         startDate: LocalDate?,
         endDate: LocalDate?,
-        refundReason: RefundReason?,
+        refundStatus: RefundStatus?,
         userId: Long?,
         pageable: Pageable
     ): Page<Refund>
