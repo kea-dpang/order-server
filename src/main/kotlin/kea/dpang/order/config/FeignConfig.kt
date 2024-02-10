ca7695683d73c2dc5097c91faf810d1fa602bcc3
SeungHyeon <namsh1125@naver.com>
2024-02-10 22:26:36 +0900

    Feat: Feign 클라이언트에 'X-DPANG-SERVICE-NAME' 헤더 인터셉터 추가

--- src/main/kotlin/kea/dpang/order/config/FeignConfig.kt
+++ src/main/kotlin/kea/dpang/order/config/FeignConfig.kt
@@ -1,13 +1,23 @@
 package kea.dpang.order.config
 
 import feign.Logger
+import feign.RequestInterceptor
+import feign.RequestTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class FeignConfig {
+
     @Bean
     fun feignLoggerLevel(): Logger.Level {
         return Logger.Level.FULL
+    }
+
+    @Bean
+    fun headerInterceptor(): RequestInterceptor {
+        return RequestInterceptor { requestTemplate: RequestTemplate ->
+            requestTemplate.header("X-DPANG-SERVICE-NAME", "ORDER-SERVICE")
+        }
     }
 }
